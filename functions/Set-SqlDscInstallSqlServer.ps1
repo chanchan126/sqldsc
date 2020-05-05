a1ef6621057119c48b3312bc2aad626c8868d143
Christian Duque <ChristianD@datacom.co.nz>
2020-05-06 10:59:27 +1200

    update install sql server scrupt

--- functions/Set-SqlDscInstallSqlServer.ps1
+++ functions/Set-SqlDscInstallSqlServer.ps1
@@ -7,6 +7,8 @@
         String containing the location of the setup exe file.
     .PARAMETER SQLInstanceName
         String containing the name of the instance. if value is blank, default instance will be installed
+    .PARAMETER Features
+        String containing the features to be installed. ex. 'SQLENGINE' or 'SQLENGINE,IS' or 'SQLENGINE,IS,AS'
     .PARAMETER SQLCollation
         String containing the instance collation. if value is blank, default collation is SQL_Latin1_General_CP1_CI_AS
     .PARAMETER SQLSysAdminAccounts
@@ -46,8 +48,12 @@ function Set-SqlDscInstallSqlServer
 
         [Parameter()]
         [System.String]
-        $SQLInstanceName = 'MSSQLSERVER',
+        $SQLInstanceName,
 
+        [Parameter()]
+        [System.String]
+        $Features = 'SQLENGINE',
+    
         [Parameter()]
         [System.String]
         $SQLCollation = 'SQL_Latin1_General_CP1_CI_AS',
@@ -114,88 +120,96 @@ function Set-SqlDscInstallSqlServer
         $ForceReboot
     )
 
+    try {
+        #Set InstanceName variable if not provided
+        If(!$SQLInstanceName) {
+            $SQLInstanceName = 'MSSQLSERVER'
+        }
 
-    #Set InstanceName variable if not provided
-    If(!$SQLInstanceName) {
-        $SQLInstanceName = 'MSSQLSERVER'
-    }
+        #Service Account
+        $SVCPass = ConvertTo-SecureString "$SVCpassword" -AsPlainText -Force
+        $SVCpsCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($SVCaccount, $SVCPass)
 
-    #Service Account
-    $SVCPass = ConvertTo-SecureString "$SVCpassword" -AsPlainText -Force
-    $SVCpsCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($SVCaccount, $SVCPass)
+        #SA Account
+        $SAPass = ConvertTo-SecureString "$SApassword" -AsPlainText -Force
+        $SApsCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($SAaccount, $SAPass)
 
-    #SA Account
-    $SAPass = ConvertTo-SecureString "$SApassword" -AsPlainText -Force
-    $SApsCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($SAaccount, $SAPass)
-
-    #Set TempDBfiles based on number of cores
-    $cpuCores=Get-CimInstance -ClassName 'Win32_Processor' | Select-Object -ExpandProperty 'NumberOfCores';
-    [uint32]$tempDBFilesCount = $cpuCores.Count
+        #Set TempDBfiles based on number of cores
+        $cpuCores=Get-CimInstance -ClassName 'Win32_Processor' | Select-Object -ExpandProperty 'NumberOfCores';
+        [uint32]$tempDBFilesCount = $cpuCores.Count
     
-    if ($tempDBFilesCount.Count -gt 8) {
-        [uint32]$tempDBFilesCount = 8
+        if ($tempDBFilesCount.Count -gt 8) {
+            [uint32]$tempDBFilesCount = 8
+        }
+
+        #region: INSTALL SQL SERVER #
+        $SQLMajorVersion = (Get-Item -Path $(Join-Path $SQLSetupPath 'setup.exe')).VersionInfo.ProductVersion.Split('.')[0]
+        $BrowserSvcStartupType = if ($SQLInstanceName -eq 'MSSQLSERVER') { 'Disabled' } else { 'Automatic' }
+
+        If ($ForceReboot) {
+
+            $sqlSetupParams = @{
+                SourcePath             = $SQLSetupPath
+                InstanceName           = $SQLInstanceName
+                Features               = $Features
+                SQLCollation           = $SQLCollation
+                SQLSvcAccount          = $SVCpsCred
+                AgtSvcAccount          = $SVCpsCred
+                SQLSysAdminAccounts    = $SQLSysAdminAccounts
+                SecurityMode           = "SQL"
+                SAPwd                  = $SApsCred
+                InstallSharedDir       = "C:\Program Files\Microsoft SQL Server"
+                InstallSharedWOWDir    = "C:\Program Files (x86)\Microsoft SQL Server"
+                FeatureFlag            = @('DetectionSharedFeatures')
+                InstanceDir            = $SQLInstanceDrive
+                InstallSQLDataDir      = $SQLInstanceDrive
+                SQLUserDBDir           = (Join-Path $SQLDataDrive "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\DATA")
+                SQLUserDBLogDir        = (Join-Path $SQLLogDrive  "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\LOG")
+                SQLTempDBDir           = (Join-Path $SQLTempdbDrive "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\TEMPDB")
+                SQLTempDBLogDir        = (Join-Path $SQLTempdbDrive "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\TEMPDB")
+                SQLBackupDir           = (Join-Path $SQLBackupDrive  "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\BACKUP")
+                SqlTempdbFileCount     = $tempDBFilesCount
+                SqlTempdbFileSize      = $TempDBFileSize
+                SqlTempdbFileGrowth    = $TempDBFileGrowth
+                SqlTempdbLogFileSize   = $TempDBLogFileSize
+                SqlTempdbLogFileGrowth = $TempDBLogFileSizeGrowth
+                BrowserSvcStartupType  = $BrowserSvcStartupType
+        }
+
+        If($TempDBFileSize -and $TempDBFileGrowth -and $TempDBLogFileSize -and $TempDBLogFileSizeGrowth) {
+            $sqlSetupParams.Add ('TempDBFileSize',$TempDBFileSize)
+            $sqlSetupParams.Add ('TempDBFileGrowth',$TempDBFilTempDBFileGrowtheSize)
+            $sqlSetupParams.Add ('TempDBLogFileSize',$TempDBLogFileSize)
+            $sqlSetupParams.Add ('TempDBLogFileSizeGrowth',$TempDBLogFileSizeGrowth)
+        }
+
+        If ($ForceReboot) { 
+            [boolean]$ForceRB = $true
+            $sqlSetupParams.Add('ForceReboot',$ForceRB) 
+        }
+
+        If ($SQLUpdatePath) {
+            $sqlSetupParams.Add('UpdateEnabled','True')
+            $sqlSetupParams.Add('UpdateSource',$SQLUpdatePath)
+        }
+
+        If ($SQLSetupPath) {
+            $testSqlSetup = Invoke-DscResource -ModuleName SqlServerDsc -Name SqlSetup -Property $sqlSetupParams -Method Test -Verbose
+            If (!$testSqlSetup) {
+                Write-Host 'Installing(SqlSetup)...' -BackgroundColor White -ForegroundColor Black
+                Invoke-DscResource -ModuleName SqlServerDsc -Name SqlSetup -Property $sqlSetupParams -Method Set -Verbose
+            }
+              
+            Else {
+            Write-Host 'Skipping(SqlSetup)... SQL Instance already exists.' -BackgroundColor Yellow -ForegroundColor Black
+            } 
+        }
+        
+        Else {
+            Write-Host 'Skipping(SqlSetup)... No setup path provided.' -BackgroundColor Yellow -ForegroundColor Black
+        }
+    } 
+
+    Catch{
+        Write-Error "Error. Please check your command string: $_"
     }
-
-    #region: INSTALL SQL SERVER #
-    $SQLMajorVersion = (Get-Item -Path $(Join-Path $SQLSetupPath 'setup.exe')).VersionInfo.ProductVersion.Split('.')[0]
-    $BrowserSvcStartupType = if ($SQLInstanceName -eq 'MSSQLSERVER') { 'Disabled' } else { 'Automatic' }
-
-    If ($ForceReboot) {
-
-    $sqlSetupParams = @{
-        SourcePath             = $SQLSetupPath
-        InstanceName           = $SQLInstanceName
-        Features               = 'SQLENGINE'
-        SQLCollation           = $SQLCollation
-        SQLSvcAccount          = $SVCpsCred
-        AgtSvcAccount          = $SVCpsCred
-        SQLSysAdminAccounts    = $SQLSysAdminAccounts
-        SecurityMode           = "SQL"
-        SAPwd                  = $SApsCred
-        InstallSharedDir       = "C:\Program Files\Microsoft SQL Server"
-        InstallSharedWOWDir    = "C:\Program Files (x86)\Microsoft SQL Server"
-        FeatureFlag            = @('DetectionSharedFeatures')
-        InstanceDir            = $SQLInstanceDrive
-        InstallSQLDataDir      = $SQLInstanceDrive
-        SQLUserDBDir           = (Join-Path $SQLDataDrive "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\DATA")
-        SQLUserDBLogDir        = (Join-Path $SQLLogDrive  "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\LOG")
-        SQLTempDBDir           = (Join-Path $SQLTempdbDrive "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\TEMPDB")
-        SQLTempDBLogDir        = (Join-Path $SQLTempdbDrive "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\TEMPDB")
-        SQLBackupDir           = (Join-Path $SQLBackupDrive  "MSSQL$SQLMajorVersion.$SQLInstanceName\MSSQL\BACKUP")
-        SqlTempdbFileCount     = $tempDBFilesCount
-        SqlTempdbFileSize      = $TempDBFileSize
-        SqlTempdbFileGrowth    = $TempDBFileGrowth
-        SqlTempdbLogFileSize   = $TempDBLogFileSize
-        SqlTempdbLogFileGrowth = $TempDBLogFileSizeGrowth
-        BrowserSvcStartupType  = $BrowserSvcStartupType
-}
-
-If($TempDBFileSize -and $TempDBFileGrowth -and $TempDBLogFileSize -and $TempDBLogFileSizeGrowth) {
-    $sqlSetupParams.Add ('TempDBFileSize',$TempDBFileSize)
-    $sqlSetupParams.Add ('TempDBFileGrowth',$TempDBFilTempDBFileGrowtheSize)
-    $sqlSetupParams.Add ('TempDBLogFileSize',$TempDBLogFileSize)
-    $sqlSetupParams.Add ('TempDBLogFileSizeGrowth',$TempDBLogFileSizeGrowth)
-}
-
-If ($ForceReboot) { 
-    [boolean]$ForceRB = $true
-    $sqlSetupParams.Add ('ForceReboot',$ForceRB) }
-
-If ($SQLUpdatePath) {
-    $sqlSetupParams.Add ('UpdateEnabled','True')
-    $sqlSetupParams.Add ('UpdateSource',$SQLUpdatePath)
-}
-
-If ($SQLSetupPath) {
-    $testSqlSetup = Invoke-DscResource -ModuleName SqlServerDsc -Name SqlSetup -Property $sqlSetupParams -Method Test -Verbose
-    If (!$testSqlSetup) {
-        Write-Host 'Installing(SqlSetup)...' -BackgroundColor White -ForegroundColor Black
-        Invoke-DscResource -ModuleName SqlServerDsc -Name SqlSetup -Property $sqlSetupParams -Method Set -Verbose
-    } 
-    Else {
-        Write-Host 'Skipping(SqlSetup)... SQL Instance already exists.' -BackgroundColor Yellow -ForegroundColor Black
-    } 
-} 
-Else {
-    Write-Host 'Skipping(SqlSetup)... No setup path provided.' -BackgroundColor Yellow -ForegroundColor Black
-}
